404f819970c88bbc8e33e1b5c8f4ca206f050844
zidanpiaoguo <zidanpiaoguo@163.com>
2017-12-20 13:40:10 +0800

    修改了share

--- app/src/main/java/com/lzy/mywheelsfive/MainActivity.java
+++ app/src/main/java/com/lzy/mywheelsfive/MainActivity.java
@@ -1,17 +1,18 @@
 package com.lzy.mywheelsfive;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.lzy.mywheelsfive.Base.BaseActivity;
 
-import cn.sharesdk.onekeyshare.OnekeyShare;
-
 public class MainActivity extends BaseActivity {
 
     private Button mBtShare;
+    private Button mBtQQ;
+    private Button mBtQZone;
+    private Button mBtSiNa;
+    private Button mBtWZ;
+    private Button mBtFriend;
 
     @Override
     protected int setLayoutId() {
@@ -21,6 +22,11 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
         mBtShare = (Button) findViewById(R.id.bt_share);
+        mBtQQ = (Button) findViewById(R.id.bt_QQ);
+        mBtQZone = (Button) findViewById(R.id.bt_QZone);
+        mBtSiNa = (Button) findViewById(R.id.bt_SiNa);
+        mBtWZ = (Button) findViewById(R.id.bt_WZ);
+        mBtFriend = (Button) findViewById(R.id.bt_Friend);
     }
 
 
@@ -32,58 +38,42 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void setClickEvent() {
 
-        mBtShare.setOnClickListener(new View.OnClickListener() {
+
+        mBtQQ.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showShare();
+                ShareUtils.sendQQ();
+            }
+        });
+
+        mBtQZone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShareUtils.sendQzone();
+            }
+        });
+        mBtSiNa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShareUtils.sendSiNa();
+            }
+        });
+
+        mBtWZ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShareUtils.sendWeChat();
+            }
+        });
+        mBtFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShareUtils.sendWeChatCore();
             }
         });
 
     }
 
-    private void showShare() {
-        OnekeyShare oks = new OnekeyShare();
-        //关闭sso授权
-        oks.disableSSOWhenAuthorize();
 
-        // 分享时Notification的图标和文字  2.5.9以后的版本不     调用此方法
-        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
-        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-
-//        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-//        oks.setTitleUrl("http://sharesdk.cn");
-
-        // text是分享文本，所有平台都需要这个字段
-//        oks.setText("我是分享文本");
-
-
-
-        // url仅在微信（包括好友和朋友圈）中使用
-
-
-        oks.setTitle("轮子");
-//        // titleUrl是标题的网络链接，QQ和QQ空间等使用
-//        oks.setTitleUrl("https://github.com/Horrarndoo/YiZhi");
-//        // text是分享文本，所有平台都需要这个字段
-        oks.setText("每日新闻，精选干货，最新资讯，应有尽有.项目详情链接：https://github.com/Horrarndoo/YiZhi");
-
-        oks.setImageUrl("http://pic4.nipic.com/20091217/3885730_124701000519_2.jpg");
-
-//        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        oks.setUrl("https://www.baidu.com/");
-
-
-
-        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        oks.setComment("我是测试评论文本");
-        // site是分享此内容的网站名称，仅在QQ空间使用
-        oks.setSite(getString(R.string.app_name));
-
-        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl("http://sharesdk.cn");
-
-        // 启动分享GUI
-        oks.show(this);
-    }
 
 }
---
+++ app/src/main/java/com/lzy/mywheelsfive/ShareUtils.java
@@ -0,0 +1,187 @@
+package com.lzy.mywheelsfive;
+
+import android.util.Log;
+
+import java.util.HashMap;
+
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.sina.weibo.SinaWeibo;
+import cn.sharesdk.tencent.qq.QQ;
+import cn.sharesdk.tencent.qzone.QZone;
+import cn.sharesdk.wechat.friends.Wechat;
+import cn.sharesdk.wechat.moments.WechatMoments;
+
+/**
+ * Created by bullet on 2017/12/19.
+ */
+
+public class ShareUtils {
+    private static final String TAG = "ShareUtils";
+
+    public static void sendQzone() {
+
+        Platform.ShareParams sp = new Platform.ShareParams();
+        //必须带的四个参数
+        sp.setTitle("测试分享的标题");
+        sp.setTitleUrl("http://www.baidu.com"); // 标题的超链接
+        sp.setSite("发布分享的网站名称");
+        sp.setSiteUrl("发布分享网站的地址");
+
+        sp.setText("测试分享的文本");
+        sp.setImageUrl("http://www.someserver.com/测试图片网络地址.jpg");
+        Platform qzone = ShareSDK.getPlatform(QZone.NAME);
+        // 设置分享事件回调（注：回调放在不能保证在主线程调用，不可以在里面直接处理UI操作）
+        qzone.setPlatformActionListener(new PlatformActionListener() {
+            public void onError(Platform arg0, int arg1, Throwable arg2) {
+                //失败的回调，arg:平台对象，arg1:表示当前的动作，arg2:异常信息
+                Log.d(TAG, "onError: "+"Qzone");
+            }
+
+            public void onComplete(Platform arg0, int arg1, HashMap arg2) {
+                //分享成功的回调
+
+                Log.d(TAG, "onComplete: "+"Qzone");
+            }
+
+            public void onCancel(Platform arg0, int arg1) {
+                //取消分享的回调
+                Log.d(TAG, "onCancel: "+"Qzone");
+            }
+        });
+        // 执行图文分享
+        qzone.share(sp);
+    }
+
+
+    public static void sendQQ(){
+        Platform.ShareParams sp = new Platform.ShareParams();
+        sp.setTitle("测试分享的标题");
+        sp.setTitleUrl("http://www.baidu.com"); // 标题的超链接
+        sp.setText("测试分享的文本");
+        sp.setImageUrl("http://pic4.nipic.com/20091217/3885730_124701000519_2.jpg");
+
+        Platform qq = ShareSDK.getPlatform(QQ.NAME);
+        // 设置分享事件回调（注：回调放在不能保证在主线程调用，不可以在里面直接处理UI操作）
+        qq.setPlatformActionListener(new PlatformActionListener() {
+            public void onError(Platform arg0, int arg1, Throwable arg2) {
+                //失败的回调，arg:平台对象，arg1:表示当前的动作，arg2:异常信息
+            }
+
+            public void onComplete(Platform arg0, int arg1, HashMap arg2) {
+                //分享成功的回调
+            }
+
+            public void onCancel(Platform arg0, int arg1) {
+                //取消分享的回调
+            }
+        });
+        // 执行图文分享
+        qq.share(sp);
+    }
+
+
+    public static void sendSiNa(){
+
+        Platform.ShareParams sp = new Platform.ShareParams();
+        sp.setText("测试分享的文本");
+        sp.setImageUrl("http://pic4.nipic.com/20091217/3885730_124701000519_2.jpg");
+
+        Platform weibo = ShareSDK.getPlatform(SinaWeibo.NAME);
+        weibo.setPlatformActionListener(new PlatformActionListener() {
+            @Override
+            public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+
+            }
+
+            @Override
+            public void onError(Platform platform, int i, Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCancel(Platform platform, int i) {
+
+            }
+        }); // 设置分享事件回调
+        // 执行图文分享
+        weibo.share(sp);
+    }
+
+
+    /**
+     * 分享微信好友
+     */
+    public static void sendWeChat(){
+
+
+        Platform.ShareParams sp = new Platform.ShareParams();
+        sp.setTitle("标题");
+        sp.setText("测试分享的文本");
+
+        sp.setImageUrl("http://pic4.nipic.com/20091217/3885730_124701000519_2.jpg");
+        sp.setUrl("http://www.baidu.com");
+
+        Platform weibo = ShareSDK.getPlatform(Wechat.NAME);
+        weibo.setPlatformActionListener(new PlatformActionListener() {
+            @Override
+            public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+
+            }
+
+            @Override
+            public void onError(Platform platform, int i, Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCancel(Platform platform, int i) {
+
+            }
+        }); // 设置分享事件回调
+        // 执行图文分享
+        weibo.share(sp);
+    }
+
+
+
+    /**
+     * 分享微信好友
+     */
+    public static void sendWeChatCore(){
+
+
+        Platform.ShareParams sp = new Platform.ShareParams();
+        sp.setTitle("标题");
+        sp.setText("测试分享的文本");
+
+        sp.setImageUrl("http://pic4.nipic.com/20091217/3885730_124701000519_2.jpg");
+        sp.setUrl("http://www.baidu.com");
+
+        Platform weibo = ShareSDK.getPlatform(WechatMoments.NAME);
+        weibo.setPlatformActionListener(new PlatformActionListener() {
+            @Override
+            public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+
+            }
+
+            @Override
+            public void onError(Platform platform, int i, Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCancel(Platform platform, int i) {
+
+            }
+        }); // 设置分享事件回调
+        // 执行图文分享
+        weibo.share(sp);
+    }
+
+
+
+
+
+}
